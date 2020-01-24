4b7d4b4ae467430f63e84b3c7d9c221e693ccfb6
anthony <anthonycolvil@gmail.com>
2020-01-24 19:22:47 -0400

    page de co

--- wibash/ContentView.swift
+++ wibash/ContentView.swift
@@ -9,9 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name : String = ""
+    @State var password : String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment : .center){
+            Text("WI-Bash")
+                .font(.title)
+                .foregroundColor(.red)
+                .frame(width: 400.0, height: 200.0)
+                .background(Color.black)
+            
+            Spacer()
+            Spacer()
+            
+            TextField("Login ou mail", text: $name)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(15.0)
+                
+            SecureField("Mot de passe", text: $password)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(15.0)
+            
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("connexion")
+                    .frame(width: 150, height: 30)
+                    .foregroundColor(.white)
+                    .background(Color.orange)
+                    
+            }
+            Spacer()
+            Spacer()
+            
+        }
+            .padding()
+        
+        
     }
+    
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
