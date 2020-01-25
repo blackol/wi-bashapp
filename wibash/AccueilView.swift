5212b4a23cd35c859dd29490650a71b922c99586
blackol <anthonycolvil@gmail.com>
2020-01-25 14:07:03 -0400

    slide Menu fonctionnelle

--- wibash/AccueilView.swift
+++ wibash/AccueilView.swift
@@ -9,18 +9,57 @@
 import SwiftUI
 
 struct AccueilView: View {
+    @State var Menu = false
+    
+    
     var body: some View {
-        GeometryReader { geometry in
-            MainView()
-                .frame(width: geometry.size.width, height:geometry.size.height )
+        let drag = DragGesture()
+            .onEnded {
+                if $0.translation.width < -100 {
+                    withAnimation{
+                        self.Menu = false
+                    }
+                }
+        }
+        return NavigationView {
+            GeometryReader { geometry in
+                
+                ZStack(alignment:.leadin){
+                    MainView(Menu: self.$Menu)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x: self.Menu ? geometry.size.width/2 : 0)
+                        .disabled(self.Menu ? true : false)
+                    if self.Menu {
+                        
+                        MenuView()
+                        .frame(width: geometry.size.width/2)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+            .gesture(drag)
+            }
+            .navigationBarTitle("side menu", displayMode: .inline)
+            .navigationBarItems(leading:(
+                Button( action: {
+                    withAnimation{
+                        self.Menu.toggle()
+                    }
+                }){
+                    Image(systemName: "line.horizontal.3")
+                        .imageScale(.large)
+                }
+            ))
         }
     }
 }
 
 struct MainView: View {
+    @Binding var Menu: Bool
     var body: some View {
         Button(action: {
-            print("open side menu")
+            withAnimation{
+             self.Menu = true
+            }
         }) {
             Text("Menu")
         }
